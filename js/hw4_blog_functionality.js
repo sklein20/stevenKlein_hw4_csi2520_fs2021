a0cfb4bc646c39efb06f2dafec40837b7d617ec3
Steven Klein <sklein2@oakland.edu>
2021-11-10 13:11:34 -0500

    modified blog.html and js file

--- js/hw4_blog_functionality.js
+++ js/hw4_blog_functionality.js
@@ -1,7 +1,38 @@
-function buttonClicked()
+// function buttonClicked()
+// {
+//     alert('Added Event Listener');
+// }
+
+// let demo = document.addEventListener('click', buttonClicked);
+
+// function idk()
+// {
+//     let btn = document.createElement("BUTTON");   // Create a <button> element
+//     btn.innerHTML = "CLICK ME";                   // Insert text
+//     document.body.appendChild(btn);
+// }
+ 
+
+//blogButton.addEventListener('click', buttonClicked);
+// let blogButton = document.querySelector("#blogButton")
+// console.log(blogButton);
+// blogButton = document.addEventListener('click', commentArea);
+blogButton.addEventListener('click', commentArea);
+
+function commentArea()
 {
-    alert('WHOA');
+    document.querySelector('#main_section').style.backgroundColor= 'red';
+    // section = document.createElement("section");
+    // section.style.width = "100px";
+    // section.style.height = "100px";
+    // section.style.background = "red";
+    // section.style.color = "white";
+    // section.innerHTML = "Hello";
+    // section.style.margin = "auto";
+    // document.body.appendChild(section);
 }
 
-let demo = document.addEventListener('click', buttonClicked);
+// blogButton.addEventListener('click', commentArea);
+// btn = document.addEventListener('click', commentArea);
+// document.getElementById("main_section").appendChild(section);
 
